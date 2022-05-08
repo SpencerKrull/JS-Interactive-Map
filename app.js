022e4ccc0a7106b02c3f6624a3ea61f3d8da79c2
Spencer Krull <spencerkrull138@gmail.com>
2022-05-08 09:50:15 -0600

    created submit bar

---
+++ app.js
@@ -0,0 +1,23 @@
+// create map
+
+
+    // build leaflet map
+
+
+    // add business markers
+
+
+// get coordinates via geolocation api
+
+
+// get foursquare businesses
+
+
+// process foursquare array
+
+
+// event handlers
+// window load
+
+
+// business submit button
